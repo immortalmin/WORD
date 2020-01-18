b223c32496853a459bd6f629fe9217e6748a5ad7
immortalmin <1608725226@qq.com>
2020-01-18 21:39:19 +0800

    shit fragment

--- word_test1/app/src/main/java/com/example/administrator/listviewadptwebjsonimg/CountDownFragment.java
+++ word_test1/app/src/main/java/com/example/administrator/listviewadptwebjsonimg/CountDownFragment.java
@@ -8,9 +8,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class CountDownFragment extends Fragment {
     private final static String TAG = "CountDownFragment";
+    private View mView;
+    private String res = "false";
+    TextView textView1;
 
     /**
      * Activity绑定上Fragment时，调用该方法
@@ -34,7 +38,20 @@ public class CountDownFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.activity_view_test,null);
+        Log.d(TAG,"onCreateView");
+        mView = view;
+        textView1 = mView.findViewById(R.id.tv1);
+        textView1.setText("YES");
+//        Log.d("gettextttttt",..toString());
+        res = "ture";
         return view;
     }
+
+    public String getV(){
+
+
+        return res;
+    }
+
 
 }
--- word_test1/app/src/main/java/com/example/administrator/listviewadptwebjsonimg/FragmentTestActivity.java
+++ word_test1/app/src/main/java/com/example/administrator/listviewadptwebjsonimg/FragmentTestActivity.java
@@ -5,20 +5,38 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FragmentTestActivity extends AppCompatActivity {
 
+    TextView tv1;
+    Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_test);
+        btn1 = (Button)findViewById(R.id.btn1);
 //        setFragment();
-//        FragmentManager fragmentManager = getSupportFragmentManager();
-//        FragmentTransaction beginTransaction = fragmentManager.beginTransaction();
-//        CountDownFragment fragment1 = new CountDownFragment();
-//        beginTransaction.replace(android.R.id.content,new CountDownFragment());
-//        beginTransaction.commit();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction beginTransaction = fragmentManager.beginTransaction();
+        final CountDownFragment fragment1 = new CountDownFragment();
 
+//        tv1=getFragmentManager().findFragmentById()
+
+        beginTransaction.replace(android.R.id.content,fragment1);
+        beginTransaction.commit();
+//        btn1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Log.i("!!!!!!!!!!!!",fragment1.getV());
+//            }
+//        });
+
+//        tv1 = .findViewById(R.id.tv1);
+//        tv1.setText("success");
     }
 
 //    private void setFragment(){
